f5742a019552858280f4f26d19fefa642b7dbecb
ikhsanramadhani <fadilatalah83@gmail.com>
2026-06-17 12:02:29 +0700

    17JUNI

---
+++ src/main/java/com/mycompany/javaaacrud/JavaaaCRUD.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.javaaacrud;
+
+/**
+ *
+ * @author Lenovo Thinkpad
+ */
+public class JavaaaCRUD {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
